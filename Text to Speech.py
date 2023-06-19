9e41649d9162c6996dfa60ec14a1b45827ac7b88
Saubhik Ghosh <76199977+novamorningstar@users.noreply.github.com>
2023-06-19 23:31:08 +0530

    Update Text to Speech.py

--- Text to Speech.py
+++ Text to Speech.py
@@ -3,5 +3,5 @@ from playsound import playsound
 text = "I don't know" #Enter the text you want to convert 
 language = "en" #the language you want to convert to
 obj = gTTS(text=text, lang=language, slow=False)
-obj.save("filename.mp3")
+obj.save("filename.mp3") #the file name you want to save as 
 playsound("filename.mp3")
